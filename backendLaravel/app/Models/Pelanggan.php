39d95e3ffdae8ccc601ac174e8a1d73cee33a4dd
Muhammad Sabirin <ntahlah47990@gmail.com>
2025-05-25 17:39:15 +0700

    tambah isi model pelanggan

--- backendLaravel/app/Models/Pelanggan.php
+++ backendLaravel/app/Models/Pelanggan.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pelanggan extends Model
 {
-    //
+    protected $table = 'pelanggan';
+    protected $primaryKey = 'id_pelanggan';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nama',
+        'alamat',
+        'telepon',
+        'email',
+        'created_at',
+        'updated_at'
+    ];
+
+    // Define any relationships if necessary
+    // public function orders() {
+    //     return $this->hasMany(Order::class, 'pelanggan_id', 'id_pelanggan');
+    // }
 }
